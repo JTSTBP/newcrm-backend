565326bfe1bfe5097c5e03fdf4b8ee12ba2cbe61
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2026-04-10 17:01:06 +0530

    mmm

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ app.use((req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is successfully running on port ${PORT}`);
 });
